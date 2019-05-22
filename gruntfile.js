ecfbbde16f9d9020bc15da80f1dbd50c1e615e8d
Patrick Zimmerman <zimkjz@yahoo.com>
2019-05-22 15:12:56 -0600

    new team city message

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,10 @@
 module.exports = (grunt) => {
-    grunt.registerTask('default', 'Hello World', () => grunt.log.writeln("##teamcity[buildProblem description='There is a problem with Hello World' identity='123456']"))
- }
+    grunt.registerTask('default', 'Example TeamCity Progress Messaging', function() {
+        const done = this.async();
+        grunt.log.writeln("##teamcity[progressMessage 'An example progress message. Waiting 5 seconds']");
+        setTimeout(() => {
+            grunt.log.writeln("##teamcity[progressMessage '5 seconds over, all done']");
+            done();
+        }, 5000);
+    });
+ };
