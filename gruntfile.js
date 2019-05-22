65207da7c10113954c28ebdf77663b1c7606b244
Patrick Zimmerman <zimkjz@yahoo.com>
2019-05-22 15:05:47 -0600

    changed grunt for team city

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,3 @@
 module.exports = (grunt) => {
-    grunt.registerTask('default', 'Hello World', () => grunt.log.writeln('Hello World'))
+    grunt.registerTask('default', 'Hello World', () => grunt.log.writeln("##teamcity[buildProblem description='There is a problem with Hello World' identity='123456']"))
  }
